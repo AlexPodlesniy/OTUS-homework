a7e30635dc6b76d59ceb7100028cbb26756c9baf
AlexPodlesniy <apodlesnyj@yandex.ru>
2021-10-03 20:56:03 +0300

    2nd homework

--- L04-generics/src/main/java/homework/CustomerReverseOrder.java
+++ L04-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -1,10 +1,11 @@
 package homework;
 
+import java.util.Deque;
 import java.util.LinkedList;
 
 public class CustomerReverseOrder {
 
-    private final LinkedList<Customer> customers;
+    private final Deque<Customer> customers;
 
     public CustomerReverseOrder() {
         this.customers = new LinkedList<>();
